0a0894b4d2eeef36be14063d19a00a7321d551a4
kassing3 <108312371+kassing3@users.noreply.github.com>
2022-11-07 09:57:58 -0500

    Employee Test

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -3,6 +3,60 @@
 
 const Employee = require("../lib/Employee");
 
-describe ( "Employee Class"
+describe ( "Employee Class", () => {
 
-)
+    describe("Employee Class Constructor", () => {
+        it("should create an object with the provided arguments for name, id, and email", () => {
+            const employee = new Employee("Leslie", "123123", "les@comp.com");
+
+            expect(store).toBe({name:"Leslie", id:"123123", email:"les@comp.com"});
+        })
+    });
+
+    describe("getName", () => {
+
+        it("should return the given Employee's name from the employee's object", ()=> {
+            
+
+            expect(store.getName()).toBe("Leslie");
+
+        });
+
+    });
+
+    describe("getId", () => {
+
+        it("should return the given Employee's id from the employee's object", ()=> {
+            
+
+            expect(store.getId()).toBe("123123");
+
+        });
+        
+    });
+
+    describe("getId", () => {
+
+        it("should return the given Employee's email from the employee's object", ()=> {
+            
+
+            expect(store.getEmail()).toBe("les@comp.com");
+
+        });
+        
+    });
+
+    describe("getRole", () => {
+
+        it("should return the sting 'Employee'", ()=> {
+            
+
+            expect(store.getRole()).toBe("les@comp.com");
+
+        });
+        
+    });
+
+
+
+});
